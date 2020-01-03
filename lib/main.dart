581b780ea28ebc67a7ad731423fc4d019f13adeb
Sergej Atamantschuk <sergatam@yandex.com>
2020-01-03 18:44:39 +0100

    chore: init project structure

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:linote/pages/home.dart';
+
+void main() => runApp(Linote());
+
+class Linote extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Linote',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home()
+    );
+  }
+}
